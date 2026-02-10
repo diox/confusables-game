6b78be32686357dd00163a1dd2981a67e91ddd8a
Mathieu Pillard <m@virgule.net>
2026-02-10 21:37:45 +0100

    prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const basicAuth = require("express-basic-auth");
 const path = require("path");
-const db = require('better-sqlite3')('confusables.db');
+const db = require("better-sqlite3")("confusables.db");
 
 app = express();
 
@@ -10,11 +10,11 @@ app.use("/static", express.static("static", { maxAge: "365d" }));
 app.use(
   basicAuth({
     authorizer: (username, password) => {
-        // Any non-blank username/password works now.
-        return username && password;
+      // Any non-blank username/password works now.
+      return username && password;
     },
     challenge: true,
-    realm: 'confusables-game',
+    realm: "confusables-game",
   }),
 );
 
@@ -24,8 +24,15 @@ app.get("/", function (req, res) {
 });
 
 app.post("/record", express.json(), function (req, res) {
-  const statement = db.prepare('INSERT INTO confusables (timestamp, user, confusable, alphabet) VALUES (?, ?, ?, ?)');
-  statement.run(Date.now(), req.auth.user, req.body.confusable, req.body.alphabet);  
+  const statement = db.prepare(
+    "INSERT INTO confusables (timestamp, user, confusable, alphabet) VALUES (?, ?, ?, ?)",
+  );
+  statement.run(
+    Date.now(),
+    req.auth.user,
+    req.body.confusable,
+    req.body.alphabet,
+  );
   res.status(202).send();
 });
 
